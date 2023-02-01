8282dbc4cf25df40fa635cb3bb6950714927ef98
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2023-02-01 23:57:26 +0530

    Create cnt_nodes_with_degree2.cpp

---
+++ cnt_nodes_with_degree2.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct Node {
+	int data;
+	struct Node *left, *right;
+};
+int count(struct Node *p){
+    int x,y;
+    if(p!=NULL){
+        x=count(p->left);
+        y=count(p->right);
+    if(p->left && p->right)
+        return x+y+1;
+    return x+y;
+    }
+    return 0;
+}
+Node* newNode(int data)
+{
+	Node* temp = new Node;
+	temp->data = data;
+	temp->left = temp->right = NULL;
+	return temp;
+}
+
+int main()
+{
+
+	Node* root = newNode(1);
+	root->left = newNode(2);
+	root->right = newNode(3);
+	root->left->left = newNode(4);
+	root->left->right = newNode(5);
+	root->right->left = newNode(6);
+	root->right->right = newNode(7);
+	cout << "The number of nodes with degree 2 is equal to ";
+	cout<<count(root);
+	return 0;
+}
